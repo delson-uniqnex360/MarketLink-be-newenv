47b134871477a7b0ff06c81a5c7a0006a3cbdea3
delson-uniqnex360 <delson@uniqnex@gmail.com>
2026-09-01 19:01:30 +0530

    addedcsrf ip

--- ecommerce_tool/ecommerce_tool/settings.py
+++ ecommerce_tool/ecommerce_tool/settings.py
@@ -35,6 +35,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://marketplace-frontend-development.vercel.app",
     "https://marketplace-integration-app.vercel.app",
     "https://b2bop.netlify.app",
+    "http://98.93.11.169.nip.io/",
+    "https://98.93.11.169.nip.io/",
 ]
 
 
